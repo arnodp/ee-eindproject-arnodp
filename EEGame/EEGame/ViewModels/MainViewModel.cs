4da81ccdbd4bfc99c69899408768572d8b11b8f6
arnodp <depotter.arno@gmail.com>
2018-12-12 15:13:43 +0100

    MainV_BtnCommands_views_Viewmodels

--- EEGame/EEGame/ViewModels/MainViewModel.cs
+++ EEGame/EEGame/ViewModels/MainViewModel.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 using FreshMvvm;
+using Xamarin.Forms;
 
 namespace EEGame.ViewModels
 {
     public class MainViewModel : FreshBasePageModel
     {
 
+
+        public ICommand StartGameCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<ScenarioViewModel>(true);
+            });
+        public ICommand ContinueCommand => new Command(
+            async () =>
+            {
+                //nothing yet
+            });
+        public ICommand OpenSettingsCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<SettingsViewModel>(true);
+            });
+        public ICommand OpenHistoryCommand => new Command(
+            async () =>
+            {
+                await CoreMethods.PushPageModel<HistoryViewModel>(true);
+            });
     }
 }
---
+++ EEGame/EEGame/ViewModels/ScenarioViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EEGame.ViewModels
+{
+    public class ScenarioViewModel : FreshBasePageModel
+    {
+    }
+}
